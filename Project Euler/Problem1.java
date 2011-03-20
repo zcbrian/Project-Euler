59706ec4554108c6cf3055f9b8c6c41484bb5b6d
Brian Chang <zcbrian@gmail.com>
2011-03-20 16:00:12 -0400

    Second commit
    Testing for future updates.

---
+++ Project Euler/Problem1.java
@@ -0,0 +1,9 @@
+
+public class Problem1 {
+
+	public static void main(String [ ] args){
+		System.out.println("Hello World");
+	}
+	
+	
+}
---
+++ Project Euler/Problem2.java
@@ -0,0 +1,9 @@
+
+public class Problem2 {
+
+	public static void main(String [ ] args){
+		System.out.println("Hello World2");
+	}
+	
+	
+}
